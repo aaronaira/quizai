fc3e26ca7341566e5066b9e4104e574edefd5122
aaronaira <krustof.sea@gmail.com>
2025-02-22 18:15:23 +0100

    wip: get session as prop

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link"
 import { auth } from "@/app/auth"
-import Image from "next/image"
+import { Session } from 'next-auth'
 import Avatar from "@/app/components/Avatar";
 
 
-async function NavLink() {
-    const session = await auth();
-    if (session) console.log(session)
 
+
+async function NavLink() {
+    const session: Session | null = await auth();
 
     return (
         <nav className="flex space-x-8 items-center text-sm">
-            {session ? <Avatar /> :
+            {session ? <Avatar session={session} /> :
                 <>
                     <Link href="/signin">Sign In</Link>
                     <Link href="/signup" className="hover:bg-[--main-color] hover:text-white p-2 px-5 text-[--main-color] border-[--main-color] border-2 rounded-full font-semibold">Sign Up</Link>
