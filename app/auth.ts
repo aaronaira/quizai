52b521eb8108a9f1b6b063756d8fd0cf78b85b74
aaronaira <krustof.sea@gmail.com>
2025-02-22 18:13:39 +0100

    config: added providers google and github for login

--- app/auth.ts
+++ app/auth.ts
@@ -3,11 +3,15 @@ import NextAuth from "next-auth";
 import SequelizeAdapter from "@auth/sequelize-adapter"
 import sequelize from "./lib/sequelize";
 import Credentials from "next-auth/providers/credentials";
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 import User from "@/app/models/user";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: SequelizeAdapter(sequelize),
     providers: [
+        GitHub,
+        Google,
         Credentials({
             authorize: async (credentials) => {
                 console.log(credentials)
