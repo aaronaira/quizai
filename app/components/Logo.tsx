2040b72c00f1a362ccb1b6e8f1aed2105c41fff5
aaronaira <krustof.sea@gmail.com>
2025-02-23 22:39:27 +0100

    styles: refacctor unnecessary html

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,10 +1,9 @@
 import { FileText } from 'lucide-react'
+import Link from 'next/link'
 
 function Logo() {
     return (
-        <div className="flex items-center space-x-2">
-            <FileText color='#E74C3C' size={25} /> <span className='text-3xl font-bold uppercase'>quiz ai</span>
-        </div>
+        <Link className="flex items-center space-x-2" href="/"><FileText color='#E74C3C' size={25} /> <span className='text-3xl font-bold uppercase'>quiz ai</span></Link>
     )
 }
 
