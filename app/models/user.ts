b861b33b1f5f106146f2595f8ffb965cb289fb0f
aaronaira <krustof.sea@gmail.com>
2025-02-23 22:41:40 +0100

    wip: refactored all user model with their types and atributes

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,20 +1,74 @@
-import { DataTypes } from 'sequelize';
 import sequelize from '../lib/sequelize';
+import { DataTypes, Model, Optional } from 'sequelize';
 
-const User = sequelize.define('User', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
+interface UserAttributes {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+    password?: string | null;
+    email_verified?: Date | null;
+    image?: string | null;
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> { }
+
+class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
+    declare id: string;
+    declare name: string | null;
+    declare email: string | null;
+    declare password: string | null;
+    declare image: string | null;
+
+    toJSON() {
+        return { ...this.get() };
     }
-});
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.CHAR(36),
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: DataTypes.UUIDV4, // Para generar UUID automáticamente
+        },
+        name: {
+            type: DataTypes.STRING(255),
+            allowNull: true,
+        },
+        email: {
+            type: DataTypes.STRING(255),
+            allowNull: true,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING(255),
+            allowNull: true,
+        },
+        email_verified: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        image: {
+            type: DataTypes.STRING(255),
+            allowNull: true,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'User',
+        tableName: 'users',
+        defaultScope: {
+            attributes: {
+                include: ['id', 'name', 'image', 'email', 'password']
+            }
+        },
+        timestamps: false,
+
+    }
+);
 
 export default User;
