c118c6141dd01436b6b45801e14ec2eae8f71411
aaronaira <krustof.sea@gmail.com>
2025-02-23 22:41:13 +0100

    wip: new validation for signup

--- app/lib/zod.ts
+++ app/lib/zod.ts
@@ -4,3 +4,13 @@ export const signInSchema = z.object({
     email: z.string().email(),
     password: z.string()
 })
+
+export const signUpSchema = z.object({
+    name: z.string().min(3).max(25),
+    email: z.string().email(),
+    password: z.string().min(8),
+    confirmPassword: z.string().min(8)
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Password doesn't match",
+    path: ['confirmPassword']
+})
