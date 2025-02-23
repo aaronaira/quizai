8d89fbb00b11dade8d46cfc42a7c4199ee58dbff
aaronaira <krustof.sea@gmail.com>
2025-02-23 22:37:52 +0100

    wip: logic for auth with credentials and validate data

--- app/auth.ts
+++ app/auth.ts
@@ -1,11 +1,13 @@
-import { adapter } from 'next/dist/server/web/adapter';
+import bcrypt from 'bcrypt';
 import NextAuth from "next-auth";
 import SequelizeAdapter from "@auth/sequelize-adapter"
 import sequelize from "./lib/sequelize";
 import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
-import User from "@/app/models/user";
+import { signInSchema } from "./lib/zod";
+import User from "./models/user";
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: SequelizeAdapter(sequelize),
@@ -14,25 +16,41 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         Google,
         Credentials({
             authorize: async (credentials) => {
-                console.log(credentials)
-                return {
-                    id: "1",
-                    email: "aaronaira@gmail.com"
+                const { data, success } = signInSchema.safeParse(credentials)
+                if (!success) throw new Error('User information is wrong')
+
+                const user = await User.findOne({ where: { email: data.email } })
+
+                if (!user) throw new Error("User doesn't exists")
+
+                if (user.password) {
+                    const isValidPassword = await bcrypt.compare(data.password, user.password);
+                    if (!isValidPassword) throw new Error('Password is wrong')
                 }
 
-                // const { email, password } = credentials
-
-                // const user = await User.findOne({
-                //     where: { email }
-                // })
-
-                // if (!user) {
-                //     throw new Error("User not found")
-                // };
-
-                // if (password)
+                return {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    image: user.image
+                };
             }
         })
     ],
-    session: { strategy: 'jwt' }
+    session: { strategy: 'jwt' },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id as string;  // ✅ Asegura que id es string
+            }
+            return token;
+        },
+
+        async session({ session, token }) {
+            if (session.user && typeof token.id === 'string') {
+                session.user.id = token.id;  // ✅ token.id se asigna como string
+            }
+            return session;
+        },
+    }
 })
