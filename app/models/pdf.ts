715e1576423484650ef1f605b3dda373f0541f2c
aaronaira <krustof.sea@gmail.com>
2025-03-02 13:15:33 +0100

    wip: new model for pdfs

---
+++ app/models/pdf.ts
@@ -0,0 +1,65 @@
+// models/PDF.ts
+import { Sequelize, DataTypes, Model, Optional } from 'sequelize';
+
+// Definir atributos del modelo
+interface PDFAttributes {
+    id: string;
+    name: string;
+    hash: string;
+    size: number;
+    content: string;
+    userId: string;
+}
+
+// Especificar qué atributos son opcionales al crear un PDF
+interface PDFCreationAttributes extends Optional<PDFAttributes, 'id'> { }
+
+class PDF extends Model<PDFAttributes, PDFCreationAttributes> implements PDFAttributes {
+    public id!: string;
+    public name!: string;
+    public hash!: string;
+    public size!: number;
+    public content!: string;
+    public userId!: string;
+}
+
+export default (sequelize: Sequelize) => {
+    PDF.init(
+        {
+            id: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                primaryKey: true,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            hash: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            content: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            },
+            size: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            userId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+            },
+        },
+        {
+            sequelize,
+            modelName: 'PDF',
+            tableName: 'pdfs', // Nombre explícito de la tabla en la BD
+            timestamps: true, // Agrega createdAt y updatedAt automáticamente
+        }
+    );
+
+    return PDF;
+};
