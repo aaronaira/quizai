206eb871e5e5bae9fffeb6e746566b252597edd5
aaronaira <krustof.sea@gmail.com>
2025-03-04 11:56:22 +0100

    wip: layout profile

---
+++ app/profile/layout.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname } from 'next/navigation'
+import { User, BookOpenCheck, Settings } from 'lucide-react'
+
+export default function ProfileLayout({ children }: { children: React.ReactNode }) {
+    const currentPath = usePathname();
+    const menus = [
+        { name: "Profile", path: "/profile", icon: <User className="mr-2" /> },
+        { name: "Quizzes", path: "/profile/quizzes", icon: <BookOpenCheck className="mr-2" /> },
+        { name: "Settings", path: "/profile/settings", icon: <Settings className="mr-2" /> }
+    ]
+    return (
+        <div className="flex w-full justify-center min-h-screen overflow-hidden">
+            <div className="flex mt-40 min-w-[1024px]">
+                <aside className="w-1/4 max-h-fit relative p-6 bg-white before:absolute before:top-0 before:right-0 before:w-1 before:h-full before:bg-gray-100 before:shadow-lg">
+                    <ul className="flex flex-col gap-y-2">
+                        {menus.map(({ name, path, icon }) =>
+                            <li key={path} className={`hover:bg-slate-100 rounded-sm p-2 ${path === currentPath ? 'bg-slate-100' : 'bg-transparent'}`}><Link className="flex items-center" href={path}>{icon}{name}</Link></li>
+
+                        )}
+
+                    </ul>
+                </aside>
+
+                {/* Contenido dinámico */}
+                <div className="flex flex-col p-4 w-3/4">
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+}
