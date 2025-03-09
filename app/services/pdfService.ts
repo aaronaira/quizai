7c7557b8136d419aca0cc94e04d32133f461a54a
aaronaira <krustof.sea@gmail.com>
2025-03-09 12:30:04 +0100

    ref: pagination on get and remove

--- app/services/pdfService.ts
+++ app/services/pdfService.ts
@@ -7,9 +7,26 @@ import { models } from '@/app/models';
 
 const BASE_DIR = path.join(process.cwd(), "uploads");
 
-export async function getAll(userId: string) {
-    const pdfs = await models.PDF.findAll({ where: { userId } })
-    return pdfs;
+export async function getAll(userId: string, page = 1, limit: number = 10) {
+    try {
+        const offset = (page - 1) * limit;
+
+        const { count, rows } = await models.PDF.findAndCountAll(
+            {
+                where: { userId },
+                offset,
+                limit
+            }
+        )
+
+        return {
+            total: count,
+            data: rows,
+        };
+
+    } catch (error) {
+        throw new Error(`Internal Server Error: ${error}`)
+    }
 }
 
 export async function _delete(userId: string, hash: string) {
@@ -24,11 +41,28 @@ export async function _delete(userId: string, hash: string) {
 
         if (!pdf) throw new Error('PDF not found');
         const filePath = path.join(userPath, pdf.name)
-        const deleteFile = await unlink(filePath)
 
-        return true;
+        return deleteFile(filePath).then(async result => {
+            if (result) {
+                await pdf.destroy()
+                return { success: 'file removed successfully from our servers' }
+            }
+
+            return { error: 'PDF not found' }
+        })
+
     } catch (error) {
-        throw new Error(`Internal Server Error: ${error}`)
+        console.log(error)
+        throw new Error(`Internal Server: ${error}`)
+    }
+}
+
+async function deleteFile(filePath: string) {
+    try {
+        await fs.unlink(filePath);
+        return true;
+    } catch (error: any) {
+        return false;
     }
 }
 
