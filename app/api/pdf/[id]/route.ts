ae4afb8fe30408c78c9e301828b944aca34a52c4
aaronaira <krustof.sea@gmail.com>
2025-03-09 12:28:31 +0100

    refactored

--- app/api/pdf/[id]/route.ts
+++
@@ -1,8 +0,0 @@
-export async function DELETE(
-    request: Request,
-    { params }: { params: Promise<{ id: string }> }
-) {
-    const { id } = await params // 'a', 'b', or 'c'
-    console.log("ID =>", id)
-
-}
