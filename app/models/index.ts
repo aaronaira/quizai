57b310cfb69abedfd4ae5466dd7441824e9b53e4
aaronaira <krustof.sea@gmail.com>
2025-03-15 12:46:01 +0100

    wip: added new model quiz

--- app/models/index.ts
+++ app/models/index.ts
@@ -2,15 +2,19 @@
 import sequelize from '../lib/sequelize';
 import User from './user';
 import PDF from './pdf';
+import Quiz from './quiz';
 
 
 const models = {
     User: User(sequelize),
     PDF: PDF(sequelize),
+    Quiz: Quiz(sequelize)
 };
 
 models.User.hasMany(models.PDF, { foreignKey: 'userId', onDelete: 'CASCADE' });
 models.PDF.belongsTo(models.User, { foreignKey: 'userId' });
+models.PDF.hasMany(models.Quiz, { foreignKey: 'pdfId', onDelete: 'CASCADE' })
+models.Quiz.belongsTo(models.PDF, { foreignKey: 'pdfId' })
 
 export { sequelize, models };
 
