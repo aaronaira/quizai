472b83e7d1184a3551e8e4d5bf9ed1d019256f45
aaronaira <krustof.sea@gmail.com>
2025-03-29 15:29:46 +0100

    fix: config eslit deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  ...compat.config({
+    extends: ['next', "next/core-web-vitals", "next/typescript"],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  }),
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
