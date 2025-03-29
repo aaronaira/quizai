1b479885136d811188f318e4dc82078aa138c4d5
aaronaira <krustof.sea@gmail.com>
2025-03-29 16:19:27 +0100

    fix: issue with mysql2 package

--- app/lib/sequelize.ts
+++ app/lib/sequelize.ts
@@ -4,6 +4,7 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: "mysql",
+    dialectModule: require('mysql2'),
     logging: false
 });
 
