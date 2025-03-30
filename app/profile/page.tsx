83885a183f176fbd00a35bb86de3ac7b6c2ae98e
aaronaira <krustof.sea@gmail.com>
2025-03-30 15:40:00 +0200

    fix: route to profile

---
+++ app/profile/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pdfs from './pdfs/page'
+
+function Profile() {
+    return (
+        <div>
+            <Pdfs />
+        </div>
+    )
+}
+
+export default Profile
